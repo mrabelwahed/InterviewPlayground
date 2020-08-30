79a905028fe0bcb497bed67ec08d3797cd18b2fe
Mahmoud Ramadan <eng.mahmoudramadan2012@gmail.com>
2020-08-30 21:30:23 +0200

    finish Activity state change questions

--- README.md
+++ README.md
@@ -15,3 +15,14 @@
   
   - Answer : check this image ![Android Life Cycle](https://media.geeksforgeeks.org/wp-content/uploads/20191125165133/Activity-Lifecycle-in-Android.jpg)
   
+ ## Activity State Change
+ - When onSavedInstance(Bundle savedInstance) is called?
+ - Does onSavedInstance method is called when i call finis() method in activity?
+ - Does onSavedInstance method is called when i press Back Button?
+ - Does save heavy objects or large data set in saved instance or primitives data only?
+ - which methods can you use to restore savedInstance object ?
+ - When system call onRestoreInstanceState method?
+ - what will happen when add Edit text without id and rotate the screen?
+ 
+ -Answer : check this link ![Saving UI State](https://developer.android.com/guide/components/activities/activity-lifecycle#java)
+  
--- app/src/main/java/com/ramadan/interviewplayground/FirstActivity.java
+++ app/src/main/java/com/ramadan/interviewplayground/FirstActivity.java
@@ -1,24 +1,48 @@
 package com.ramadan.interviewplayground;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class FirstActivity extends AppCompatActivity {
 
-    final static  String TAG = "first_activity";
-
+   private  final static  String TAG = "first_activity";
+   private final static   String COUNTER="score";
+   private Button increase,decrease;
+   private TextView counterText;
+   private int counter =0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
         Log.e(TAG,"onCreate");
+        increase =  findViewById(R.id.increment);
+        decrease =  findViewById(R.id.decrease);
+        counterText = findViewById(R.id.counterText);
 
-        finish();
+        increase.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                counter++;
+                counterText.setText(String.valueOf(counter));
+            }
+        });
+
+        decrease.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                counter--;
+                counterText.setText(String.valueOf(counter));
+            }
+        });
 
         Button nextBtn = findViewById(R.id.next_btn);
         nextBtn.setOnClickListener(new View.OnClickListener() {
@@ -27,6 +51,22 @@ public class FirstActivity extends AppCompatActivity {
                 startActivity(new Intent(getApplicationContext(), SecondActivity.class));
             }
         });
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.e(TAG,"onSaveInstanceState");
+        outState.putInt(COUNTER , counter);
+    }
+
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        Log.e(TAG,"onRestoreInstanceState");
+        counter = savedInstanceState.getInt(COUNTER);
+        counterText.setText(String.valueOf(counter));
     }
 
     @Override
